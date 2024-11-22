180489a27d12b0dc9ad4dfeb46f0647bc34e7730
Alsycode <120390865+Alsycode@users.noreply.github.com>
2024-11-22 12:15:13 +0530

    2nd commit

--- client/src/pages/Products/Home/Home.jsx
+++ client/src/pages/Products/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Categories from '../../components/Categories/Categories'
-import Contact from '../../components/Contact/Contact'
-import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
-import Slider from '../../components/Slider/Slider'
+import Categories from '../../../components/Categories/Categories'
+import Contact from '../../../components/Contact/Contact'
+import FeaturedProducts from '../../../components/FeaturedProducts/FeaturedProducts'
+import Slider from '../../../components/Slider/Slider'
 import "./Home.scss"
 const Home = () => {
   return (
